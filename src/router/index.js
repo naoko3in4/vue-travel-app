e22f4937439565c03de05af9c2b3f5c9c4b71895
Naoko3in4 <okashimochi@gimail.com>
2020-03-10 16:21:23 +0900

    made control of scroll behavior

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,68 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
-
+import store from "@/store"
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
-    name: "Home",
-    component: Home
+    name: "home",
+    component: Home,
+    props: true
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    path: "/destination/:slug",
+    name: "DestinationDetails",
+    props: true,
+    component: () => import(/* webpackChunkName: "panama" */ "../views/DestinationDetails.vue"),
+    children: [
+      {
+        path: ":experienceSlug",
+        name: "experienceDetails",
+        props: true,
+        component: () => import(/*webpackChunkName: "ExperienceDeteils" */ "../views/ExperienceDetails")
+      }
+    ],
+    beforeEnter: (to,from,next) => {
+      const exists = store.destinations.find(
+        destination => destination.slug === to.params.slug
+      )
+      if(exists) {
+        next()
+      }
+      else {
+        next({ name: "notFound" })
+      }
+    }
+  },
+  {
+    path: "/404",
+    alias: "*",
+    name: "notFound",
+    component: () => import(/*webpackChunkName: "notFound" */ "../views/NotFound")
   }
 ];
 
 const router = new VueRouter({
+  mode: "history",
+  scrollBehavior(to, from, savedPosition) {
+    if(savedPosition) {
+      return savedPosition
+    } else {
+      const position = {}
+      if (to.hash) {
+        position.selector = to.hash
+        if (to.hash === '#experience') {
+          position.offset = {y:140}
+        }
+        if(document.querySelector(to.hash)) {
+          return position
+        }
+        return false
+      }
+    }
+  },
   routes
 });
 
